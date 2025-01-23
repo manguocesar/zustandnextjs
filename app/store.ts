59ea5486e0cfc45e10a1515bb93ec3a4d2f24cf9
Cesar <cesar.hertz@icloud.com>
2025-01-23 17:48:06 +0100

    Implement Zustand state management and update styles in Next.js app

---
+++ app/store.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+
+type CounterStore = {
+  count: number;
+  increment: () => void;
+  incrementAsync: () => Promise<void>;
+  decrement: () => void;
+};
+
+export const useCounterStore = create<CounterStore>((set) => ({
+    count: 0,
+    increment: () => {
+        set((state) => ({ count: state.count + 1 }));
+    },
+    incrementAsync: async () => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        set((state) => ({ count: state.count + 1 }));
+    },
+    decrement: () => {
+        set((state) => ({ count: state.count - 1 }));
+    },
+}));
+
+type TextStore = {
+    text: string;
+    add: () => void;
+};
+
+export const useTextStore = create<TextStore>((set) => ({
+  text: 'Base',
+  add: () => {
+    set((state) => ({ text: state.text }));
+  }
+}));
